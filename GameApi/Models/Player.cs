aac785f6beb2e63f3869657c59a4ec3d2d3afc42
longsmoke1001 <longsmoke1001@gmail.com>
2026-08-18 20:00:49 +0800

    remove gameapi

--- GameApi/Models/Player.cs
+++
@@ -1,8 +0,0 @@
-namespace GameApi.Models;
-public class Player
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public int Score { get; set; }
-    public int Level { get; set; }
-}
--- GameApi/Models/PlayerDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace GameApi.Models;
-
-public class PlayerDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public int Score { get; set; }
-    public int Level { get; set; }
-}
